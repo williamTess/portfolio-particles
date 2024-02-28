e435fdbaf9d278553713c3cc1c6c121f1b7215ba
William Tessari <william.tessari@epitech.eu>
2024-02-28 23:48:51 +0100

    navigation added

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,18 @@
+// next image
+import Image from "next/image";
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return (
+    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
+      <Image
+        src="/top-left-img.png"
+        width={400}
+        height={400}
+        priority
+        alt="splash"
+      />
+    </div>
+  );
 };
 
 export default TopLeftImg;
