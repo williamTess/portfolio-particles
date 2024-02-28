635dab1da2a5823840aba8cbf6d55678355fe46c
William Tessari <william.tessari@epitech.eu>
2024-02-29 00:33:11 +0100

    add config.tailwind

--- components/Transition.js
+++ components/Transition.js
@@ -17,31 +17,34 @@ const transitionVariants = {
   },
 };
 
+const AnimatedDiv = ({ className, transition }) => {
+  return (
+    <motion.div
+      className={
+        "fixed top-0 bottom-0 right-full w-screen h-screen " + className
+      }
+      variants={transitionVariants}
+      initial={"initial"}
+      animate={"animate"}
+      exit={"exit"}
+      transition={transition}
+    />
+  );
+};
+
 const Transition = () => {
   return (
     <>
-      <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
-        variants={transitionVariants}
-        initial={"initial"}
-        animate={"animate"}
-        exit={"exit"}
+      <AnimatedDiv
+        className="z-30 bg-animated1"
         transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
       />
-      <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
-        variants={transitionVariants}
-        initial={"initial"}
-        animate={"animate"}
-        exit={"exit"}
+      <AnimatedDiv
+        className="z-20 bg-animated2"
         transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
       />
-      <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
-        variants={transitionVariants}
-        initial={"initial"}
-        animate={"animate"}
-        exit={"exit"}
+      <AnimatedDiv
+        className="z-10 bg-animated3"
         transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
       />
     </>
