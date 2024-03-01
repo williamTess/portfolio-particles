475864cb99b534dc14b30b3385519cffc022992c
William Tessari <william.tessari@epitech.eu>
2024-03-02 00:18:04 +0200

    About pages done ^

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,31 @@
+// next image
+import Image from "next/image";
+
+// next link
+import Link from "next/link";
+
+// icons
+import { HiArrowRight } from "react-icons/hi2";
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto xl:mx-0 z-50">
+      <Link
+        href={"/work"}
+        className="relative w-[185px] h-[185px] flex justify-center items-center 
+        bg-circleStar bg-cover bg-center bg-no-repeat group"
+      >
+        <Image
+          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+          src={"/rounded-text.png"}
+          width={141}
+          height={148}
+          alt="job rounded"
+        />
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,36 @@
+// link
+import Link from "next/link";
+
+// icons
+import {
+  RiDribbbleLine,
+  RiFacebookBoxLine,
+  RiFacebookLine,
+  RiInstagramLine,
+  RiLinkedinLine,
+  RiYoutubeLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiDribbbleLine />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
